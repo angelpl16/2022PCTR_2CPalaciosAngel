c7c5d5657a64034a00b40c253d900a934b1b561d
Ángel Palacios López <xpl1001@alu.ubu.es>
2023-06-02 23:24:11 +0200

    Clase ActividadEnemigo

---
+++ src/p2c/p01/ActividadEnemiga.java
@@ -0,0 +1,40 @@
+package p2c.p01;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ActividadEnemiga implements Runnable{
+	
+	static Random numAleatorio = new Random();
+	
+	
+	//Se creará un numero aleatorio de enemigos que ira desde los 0 a los 200
+	private static final int MAX_ENEMIGOS = (int)numAleatorio.nextInt(200);
+	private int tipo;
+	private IJuego juego;
+	
+	public ActividadEnemiga(int tipo, IJuego juego) {
+		this.tipo = tipo;
+		this.juego = juego;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < MAX_ENEMIGOS; i++) {
+			try {
+				//Se crea un enemigo cuando tengamos la funcion
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+				
+			} catch (InterruptedException e) {
+					Logger.getGlobal().log(Level.INFO, "Se interrumpio la creacion de enemigos");
+					Logger.getGlobal().log(Level.INFO, e.toString());
+					return;
+			}
+		}
+		
+		
+	}
+
+}
---
+++ src/p2c/p01/IJuego.java
@@ -0,0 +1,9 @@
+package p2c.p01;
+
+public interface IJuego {
+	
+	public abstract void generarEnemigo(int tipo);
+
+	public abstract void eliminarEnemigo(int tipo);
+
+}
